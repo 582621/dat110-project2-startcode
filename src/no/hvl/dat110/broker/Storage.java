0043a0c0f4fa250bfc71edf2003d8e310b798175
Even <Even@DESKTOP-SQ34OK2>
2022-03-07 16:55:33 +0100

    added tests

--- src/no/hvl/dat110/broker/Storage.java
+++ src/no/hvl/dat110/broker/Storage.java
@@ -2,10 +2,14 @@ package no.hvl.dat110.broker;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import no.hvl.dat110.common.TODO;
+import no.hvl.dat110.messages.Message;
+import no.hvl.dat110.messages.PublishMsg;
 import no.hvl.dat110.common.Logger;
 import no.hvl.dat110.messagetransport.Connection;
 
@@ -20,6 +24,9 @@ public class Storage {
 	
 	protected ConcurrentHashMap<String, ClientSession> clients;
 
+	// Buffer implementation for discunnected users
+	protected ConcurrentHashMap<String, Queue<Message>> bufferedMessages;
+		
 	public Storage() {
 		subscriptions = new ConcurrentHashMap<String, Set<String>>();
 		clients = new ConcurrentHashMap<String, ClientSession>();
@@ -35,6 +42,41 @@ public class Storage {
 
 	}
 
+	public Queue<Message> getBufferedMessages(String user) {
+		return bufferedMessages.get(user);
+	}
+
+	/**
+	 * Initializes a new buffer queue for a user.
+	 * @param user
+	 */
+	public void newMessageQueue(String user) {
+		bufferedMessages.put(user, new LinkedList<Message>());
+	}
+
+	/**
+	 * Adds a message to queue of all disconnected users.
+	 * @param msg 
+	 */
+	public void addMessageToQueue (PublishMsg msg) {
+		Set<String> users = subscriptions.get(msg.getTopic());
+		if (users != null) {
+			users.stream().forEach(user -> {
+				if (bufferedMessages.containsKey(user)) {
+					Queue<Message> messages = bufferedMessages.get(user);
+					messages.add(msg);
+					bufferedMessages.put(user, messages);
+				}
+			});
+		}
+	}
+
+	/**
+	 * Clears the message queue of a user.
+	 */
+	public void clearMessageQueue(String user) {
+		bufferedMessages.remove(user);
+	}
 	// get the session object for a given user
 	// session object can be used to send a message to the user
 	
